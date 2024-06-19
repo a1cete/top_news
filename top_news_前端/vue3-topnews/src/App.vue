b1e1ffdb40048ddffbbe3912e2e3b81f5d5ec389
sakura <nixin130@outlook.com>
2024-06-19 19:16:43 +0800

    最终

--- top_news_前端/vue3-topnews/src/App.vue
+++ top_news_前端/vue3-topnews/src/App.vue
@@ -6,17 +6,20 @@
 </template>
 <script>
 import Header from './components/Header.vue'
-import { defineComponent } from 'vue'
-import { useUserInfoStore } from './stores/userInfo'
+import {defineComponent} from 'vue'
+import {useUserInfoStore} from './stores/userInfo'
 import pinia from './stores/index';
+
 const userInfoStore = useUserInfoStore(pinia)
 export default defineComponent({
   name: 'App'
 })
 </script>
 <script setup>
-import { computed, onMounted } from "vue"
-import { useRoute } from 'vue-router'
+import {computed, onMounted} from "vue"
+import {useRoute} from 'vue-router'
+import Dialog from "./components/Dialog.vue";
+
 const route = useRoute() // 路由信息对象
 // 判断是否显示header组件
 const isHeader = computed(() => {
